bbbfde9a25d84bcbfcbf625ab882354db6f6db69
Rosa Valenzuela <natsukiarr@gmail.com>
2026-02-08 20:03:23 -0700

    ADD: nullable search for session in auth-hook

--- src/shared/infrastructure/fastify/auth-hook.ts
+++ src/shared/infrastructure/fastify/auth-hook.ts
@@ -4,8 +4,7 @@ export const requireAuth = async (
   request: FastifyRequest,
   reply: FastifyReply,
 ) => {
-  if (!request.session.user) {
-    reply.status(401).send({ message: "Unauthorized" });
-    return;
+  if (!request?.session?.user) {
+    return reply.status(401).send({ message: "Unauthorized" });
   }
 };
