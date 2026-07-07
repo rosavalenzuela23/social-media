fbbc2957eaacef2fa2c065f289d1cc88bffac132
Rosa Valenzuela <natsukiarr@gmail.com>
2026-07-06 17:25:55 -0700

    change: official name for the app

--- apps/frontend/src/shared/components/NavBar.vue
+++ apps/frontend/src/shared/components/NavBar.vue
@@ -1,30 +1,30 @@
 <template>
-  <nav class="navbar w-full bg-base-100 shadow-md fixed z-1">
-    <label for="my-drawer-4" aria-label="open sidebar" class="btn btn-square btn-ghost">
-      <!-- Sidebar toggle icon -->
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        stroke-linejoin="round"
-        stroke-linecap="round"
-        stroke-width="2"
-        fill="none"
-        stroke="currentColor"
-        class="my-1.5 inline-block size-4"
-      >
-        <path
-          d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z"
-        ></path>
-        <path d="M9 4v16"></path>
-        <path d="M14 10l2 2l-2 2"></path>
-      </svg>
-    </label>
-    <div class="flex w-full justify-between">
-      <h2 class="px-4 text-bold text-2xl">SocialCat 🐈</h2>
-      <label class="input input-bordered mr-15">
-        <i class="bi bi-search"></i>
-        <input type="text" placeholder="Looking for something?" />
-      </label>
-    </div>
-  </nav>
+	<nav class="navbar w-full bg-base-100 shadow-md fixed z-1">
+		<label for="my-drawer-4" aria-label="open sidebar" class="btn btn-square btn-ghost">
+			<!-- Sidebar toggle icon -->
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 24 24"
+				stroke-linejoin="round"
+				stroke-linecap="round"
+				stroke-width="2"
+				fill="none"
+				stroke="currentColor"
+				class="my-1.5 inline-block size-4"
+			>
+				<path
+					d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z"
+				></path>
+				<path d="M9 4v16"></path>
+				<path d="M14 10l2 2l-2 2"></path>
+			</svg>
+		</label>
+		<div class="flex w-full justify-between">
+			<h2 class="px-4 text-bold text-2xl">FaceCat 🐈</h2>
+			<label class="input input-bordered mr-15">
+				<i class="bi bi-search"></i>
+				<input type="text" placeholder="Looking for something?" />
+			</label>
+		</div>
+	</nav>
 </template>
